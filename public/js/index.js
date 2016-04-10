38804db641c3e465f1548727e723fb1f72199cb2
Devviedehm <mdk.devvie@gmail.com>
2016-04-10 03:03:44 +0100

    Removed unnecessary drawing API code, apparently, and improved manually added click-location-testing.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 var map;
 var lognum=0;
-var mapDraw;
 /* inicia o map, esta função é chamada pela callback do google maps*/
 function initMap() {
 	
@@ -8,7 +7,6 @@ function initMap() {
         center: {lat: 41.1778751, lng: -8.597915999999941},
         zoom: 15
     });
-    //enableMapDraw();
     if (navigator.geolocation) {
 		log('geolocation on');
 	}
@@ -16,33 +14,6 @@ function initMap() {
 	getUserLocation();
 
 };
-
-/* activar a API de desenhos no mapa */
-function enableMapDraw(){
-	mapDraw = new google.maps.drawing.DrawingManager({
-		drawingMode: google.maps.drawing.OverlayType.MARKER,
-		drawingControl: false, //TODO: Mudar isto para False para desactivar os controlos manuais.
-		drawingControlOptions: {
-			position: google.maps.ControlPosition.TOP_CENTER,
-			drawingModes: [
-				google.maps.drawing.OverlayType.MARKER,
-				google.maps.drawing.OverlayType.CIRCLE,
-				google.maps.drawing.OverlayType.POLYGON,
-				google.maps.drawing.OverlayType.POLYLINE,
-				google.maps.drawing.OverlayType.RECTANGLE
-			]
-		},
-		circleOptions: {
-			fillColor: '#ffff00',
-			fillOpacity: 1,
-			strokeWeight: 5,
-			clickable: false,
-			editable: true,
-			zIndex: 1
-		}
-	});
-	mapDraw.setMap(map);
-}
 
 /* obter a localização do user e atualizar mapa*/
 function getUserLocation(){
@@ -267,8 +238,7 @@ function fogInner(f){
 /*Test clicking coordinates.*/
 function addClick(){
 	google.maps.event.addListener(map, 'click', function(e) {
-		console.log(e.latLng);
-		//marker = new google.maps.Marker({position: event.latLng, map: map});
+		console.log(ccc.lat()+","+ccc.lng());
 	});
 }
 
