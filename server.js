baf9bd5d554667cafc7ec95acdba00cb592b89c8
Miguel Nunes <ei12032@fe.up.pt>
2016-04-25 17:08:41 +0100

    fix

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var session = require('express-session');
 var app = express();
 app.use(session({secret: 'qvqewdxwxqeq4swrts', resave: false, saveUninitialized: false}));
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.set('views',__dirname+'/views');
+app.set('views', __dirname + '/views');
 var flash = require('connect-flash');
 app.use(flash());
 //POSTGRES
@@ -21,126 +21,124 @@ var conString = String(process.env.DATABASE_URL || "postgres://postgres:12345@lo
 /*  RESPOSTAS AOS PEDIDOS REST */
 
 /*HOMEPAGE*/
-app.get('/', function (req, res) {	
-	if (req.session.loggedin) {
-		 sendHTML(res, "index",{username: req.session.username}); // RETORNA MAPA SE ESTÁ LOGGEDIN
-		 //res.render("index",{username: req.session.username});
-		}else{	
-		sendHTML(res, "login", { message: req.flash('message') }); // SENAO OBRIGA A FAZER LOGIN
-	}	 
+app.get('/', function (req, res) {
+    if (req.session.loggedin) {
+        sendHTML(res, "index", {username: req.session.username}); // RETORNA MAPA SE ESTÁ LOGGEDIN
+        //res.render("index",{username: req.session.username});
+    } else {
+        sendHTML(res, "login", {message: req.flash('message')}); // SENAO OBRIGA A FAZER LOGIN
+    }
 });
 
 /* POST LOGIN */
-app.post('/login', urlencodedParser, function(req, res){
-	/* ISTO AINDA NÃO FAZ NADA, SÓ REDIRECT PARA O MAPA */
+app.post('/login', urlencodedParser, function (req, res) {
+    /* ISTO AINDA NÃO FAZ NADA, SÓ REDIRECT PARA O MAPA */
 
-	var username = req.body.username;
-	var password = req.body.password;
-	var client = new pg.Client(conString);
-	client.connect(function(err) {
-		if(err) {
-			return console.error('could not connect to postgres', err);
-		}
-		client.query("select * from users where username='"+username+"'", function(err, result) {
-			if(err) {
-				return console.error('error running query', err);
-			}
-			if(result.rows.length > 0){
-				if(result.rows[0].password.trim()==password){
-					req.session.loggedin=true;
-					req.session.userid=result.rows[0].userid;
-					req.session.username=result.rows[0].username;
-				}else {
-					req.session.loggedin=false;
-					req.flash('message', "Wrong password");
-				}
-			}else{
-				req.flash('message', "User not found");
-			}
-			client.end();
-			res.redirect('/');
-		});
-	});
+    var username = req.body.username;
+    var password = req.body.password;
+    var client = new pg.Client(conString);
+    client.connect(function (err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("select * from users where username='" + username + "'", function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            if (result.rows.length > 0) {
+                if (result.rows[0].password.trim() == password) {
+                    req.session.loggedin = true;
+                    req.session.userid = result.rows[0].userid;
+                    req.session.username = result.rows[0].username;
+                } else {
+                    req.session.loggedin = false;
+                    req.flash('message', "Wrong password");
+                }
+            } else {
+                req.flash('message', "User not found");
+            }
+            client.end();
+            res.redirect('/');
+        });
+    });
 });
 
 /* GET LOGOUT*/
-app.get('/logout', function(req, res){
-	req.session.loggedin=false;
-	res.redirect('/');
+app.get('/logout', function (req, res) {
+    req.session.loggedin = false;
+    res.redirect('/');
 });
 
 /* POST REGISTER */
-app.post('/register', urlencodedParser, function(req, res){
-	var username = req.body.username;
-	var email = req.body.email;
-	var password = req.body.password;
-	var confirm_password = req.body.confirm_password;
+app.post('/register', urlencodedParser, function (req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var confirm_password = req.body.confirm_password;
 
-	if (password != confirm_password) 
-	{
-		req.session.loggedin=false;
-		res.redirect('/');
-	}
-	else 
-	{
-		var client = new pg.Client(conString);
-		client.connect();
-		var query = client.query("insert into users (username,password,email) "+ 
-			"values ('"+req.body.username+"','"+
-			req.body.password+"','"+req.body.email+"')");    
-		query.on("end", function (result){          
-			client.end(); 
-			res.redirect('/');
-		});
-	}
+    if (password != confirm_password) {
+        req.session.loggedin = false;
+        res.redirect('/');
+    }
+    else {
+        var client = new pg.Client(conString);
+        client.connect();
+        var query = client.query("insert into users (username,password,email) " +
+            "values ('" + req.body.username + "','" +
+            req.body.password + "','" + req.body.email + "')");
+        query.on("end", function (result) {
+            client.end();
+            res.redirect('/');
+        });
+    }
 });
 
 /* GET especie para uma localizacao*/
 //TODO: Verificar lat+long na query?
-app.get('/api/getSpeciesFromLocation', function(req, res){
-	var lat = req.query.lat;
-	var long = req.query.long;
-	var client = new pg.Client(conString);
-	client.connect(function(err) {
-		if(err) {
-			return console.error('could not connect to postgres', err);
-		}
-		client.query("select scientificname, nomevulgar, species.specieid "
-			+"from species,	("
-			+"select distinct specieID " 
-			+"from locations "
-			+"where st_covers(location, ST_GeographyFromText('SRID=4326;POINT("+lat+" "+long+")'))"
-			+") loc where species.specieid=loc.specieid;", function(err, result) {
-				if(err) {
-					return console.error('error running query', err);
-				}
-				res.setHeader('Content-Type', 'application/json');
-				res.send(JSON.stringify({ lat: lat, long: long, result: result.rows}));
-				client.end();
-			});
-	});
+app.get('/api/getSpeciesFromLocation', function (req, res) {
+    var lat = req.query.lat;
+    var long = req.query.long;
+    var client = new pg.Client(conString);
+    client.connect(function (err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("select scientificname, nomevulgar, species.specieid "
+            + "from species,	("
+            + "select distinct specieID "
+            + "from locations "
+            + "where st_covers(location, ST_GeographyFromText('SRID=4326;POINT(" + lat + " " + long + ")'))"
+            + ") loc where species.specieid=loc.specieid;", function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({lat: lat, long: long, result: result.rows}));
+            client.end();
+        });
+    });
 });
 
 /* GET localizacoes para uma especie*/
 //TODO: Verificar se 'especie' esta definido no query? E se existem areas para a especie (caso o utilizador tenha manipulado a pagina para fazer uma query que nao devia)?
-app.get('/api/getLocationFromSpecies', function(req, res){
-	var especie = req.query.especie;
-	var client = new pg.Client(conString);
-	client.connect(function(err) {
-		if(err) {
-			return console.error('could not connect to postgres', err);
-		}
-		client.query("select ST_AsGeoJSON(location) "
-			+"from locations "
-			+"where specieid="+especie+";", function(err, result) {
-				if(err) {
-					return console.error('error running query', err);
-				}
-				res.setHeader('Content-Type', 'application/json');
-				res.send(JSON.stringify({ especie: especie, result: result.rows}));
-				client.end();
-			});
-	});
+app.get('/api/getLocationFromSpecies', function (req, res) {
+    var especie = req.query.especie;
+    var client = new pg.Client(conString);
+    client.connect(function (err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("select ST_AsGeoJSON(location) "
+            + "from locations "
+            + "where specieid=" + especie + ";", function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({especie: especie, result: result.rows}));
+            client.end();
+        });
+    });
 });
 
 /*========================================================================*/
@@ -148,14 +146,14 @@ app.get('/api/getLocationFromSpecies', function(req, res){
 var port = Number(process.env.PORT || 3000);
 var server = app.listen(port, function () {
 
-	var host = server.address().address
-	var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-	console.log("App listening at http://%s:%s", host, port)
+    console.log("App listening at http://%s:%s", host, port)
 
 });
 
-function sendHTML(res, file, data){
-	res.render(__dirname + "/public/views/"+ file, data);
-	//res.sendFile( __dirname + "/public/views/" + file);
+function sendHTML(res, file, data) {
+    res.render(__dirname + "/public/views/" + file, data);
+    //res.sendFile( __dirname + "/public/views/" + file);
 }
