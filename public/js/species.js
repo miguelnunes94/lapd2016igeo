8e44bc7246b58f425d61ea4e6bdc91f4b6f225bc
siualpinto <ei12108@fe.up.pt>
2016-06-10 17:18:28 +0000

    gbif print arrays na console

--- public/js/species.js
+++ public/js/species.js
@@ -20,26 +20,42 @@ function loadSpeciesFromLocation(lat, long){
 		data.result.forEach(function(res, i){
 			var cls = "primary";
 			if(i%2==0)
- 				cls = "success";
+				cls = "success";
 			i++;
+			
+			$.ajax( {
+				method: "GET",
+				url: "http://api.gbif.org/v1/species?name="+res.scientificname.trim().substr(0,res.scientificname.trim().indexOf(' '))+"%20"
+					+res.scientificname.trim().substr(res.scientificname.trim().indexOf(' ')+1)
+			} ).done( function(data){
+				console.log(data);
+				/*data.result.forEach( function(res,i){
+					var poly;
+					var obj = jQuery.parseJSON(res.st_asgeojson);
+					poly = obj.coordinates[0];
+					paintPoly(poly);
+					var obj = jQuery.parseJSON(res.st_asgeojson);
+					console.log(obj);
+				} );*/
+			} );
 			$("#showSpecies").append('<button data-toggle="modal" data-target="#modal_'+res.specieid+'" type="button" class="showInfo btn btn-'+cls+'">'+res.nomevulgar.trim()+' ('+res.scientificname.trim()+')</button>');
 			
 			$("#showSpecies").append('<div id="modal_'+res.specieid+'" class="modal fade" role="dialog">'+
-				  +'<div class="modal-dialog">'
-				    +'<div class="modal-content">'
-				     +'<div class="modal-header">'
-				        +'<button type="button" class="close" data-dismiss="modal">&times;</button>'
-				        +'<h4 class="modal-title">'+res.nomevulgar.trim()+' ('+res.scientificname.trim()+')</h4>'
-				      +'</div>'
-				      +'<div class="modal-body">'
-						+'<button id="btn_'+res.specieid+'">Ver no Mapa</button><br/>'
-				        +'<p>Falta por aqui a informação do GBIF (detalhes, descrição e imagem da espécie)</p>'
-				      +'</div>'
-				      +'<div class="modal-footer">'
-				        +'<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
-				      +'</div>'
-				    +'</div>'
-				  +'</div>'
+				+'<div class="modal-dialog">'
+				+'<div class="modal-content">'
+				+'<div class="modal-header">'
+				+'<button type="button" class="close" data-dismiss="modal">&times;</button>'
+				+'<h4 class="modal-title">'+res.nomevulgar.trim()+' ('+res.scientificname.trim()+')</h4>'
+				+'</div>'
+				+'<div class="modal-body">'
+				+'<button id="btn_'+res.specieid+'">Ver no Mapa</button><br/>'
+				+'<p>Falta por aqui a informação do GBIF (detalhes, descrição e imagem da espécie)</p>'
+				+'</div>'
+				+'<div class="modal-footer">'
+				+'<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
+				+'</div>'
+				+'</div>'
+				+'</div>'
 				+'</div>');
 			$("#btn_"+res.specieid).bind("click",function(){
 				clearMap();
@@ -68,9 +84,9 @@ function loadLocationFromSpecies(especie){
 }
 
 function addUserSpeciesFromLocation(lat, long) {
-    $.ajax({
-        method: "POST",
-        url: "/api/userSpeciesFromLocation",
-        data: {lat: lat, long:long}
-    });
+	$.ajax({
+		method: "POST",
+		url: "/api/userSpeciesFromLocation",
+		data: {lat: lat, long:long}
+	});
 }
